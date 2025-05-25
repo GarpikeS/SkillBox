cbaaba58f6ad71435bee4c868dcc7a8b38e827e2
GarpikeS <sivaevva@gmail.com>
2025-05-25 20:26:01 +0300

    finish task_9

--- Python Basics Part 2/Module_3_(Methods for working with lists)/task_9.py
+++ Python Basics Part 2/Module_3_(Methods for working with lists)/task_9.py
@@ -17,3 +17,34 @@
 #Размер ноги человека 2: 41
 #Размер ноги человека 3: 42
 #Наибольшее количество людей, которые могут взять ролики: 2
+
+def max_people_with_skates(skates_sizes, people_sizes):
+    skates_sizes.sort()
+    people_sizes.sort()
+    count = 0
+    i = j = 0
+    while i < len(skates_sizes) and j < len(people_sizes):
+        if skates_sizes[i] == people_sizes[j]:
+            count += 1
+            i += 1
+            j += 1
+        elif skates_sizes[i] < people_sizes[j]:
+            i += 1
+        else:
+            j += 1
+    return count
+
+rollers = int(input("Количество роликов: "))
+skates = []
+for i in range(rollers):
+    size = int(input(f"Размер пары {i + 1}: "))
+    skates.append(size)
+
+number_people = int(input("Количество людей: "))
+people = []
+for i in range(number_people):
+    size = int(input(f"Размер ноги человека {i + 1}: "))
+    people.append(size)
+
+result = max_people_with_skates(skates, people)
+print("Наибольшее количество людей, которые могут взять ролики:", result)
