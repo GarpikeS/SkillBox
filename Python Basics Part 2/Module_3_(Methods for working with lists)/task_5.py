ea36317246509970a74e0797588576261d138614
GarpikeS <sivaevva@gmail.com>
2025-05-26 17:46:33 +0300

    finish task_5

--- Python Basics Part 2/Module_3_(Methods for working with lists)/task_5.py
+++ Python Basics Part 2/Module_3_(Methods for working with lists)/task_5.py
@@ -9,3 +9,17 @@
 #Пример:
 #Изначальный список: [1, 4, –3, 0, 10]
 #Отсортированный список: [–3, 0, 1, 4, 10]
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+numbers = [1, 4, -3, 0, 10]
+print("Изначальный список:", numbers)
+insertion_sort(numbers)
+print("Отсортированный список:", numbers)
